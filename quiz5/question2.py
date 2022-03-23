5093bb660d44f65e1a8227d0a164fc43f4676403
Liana Tran <98435887+lianatr@users.noreply.github.com>
2022-03-23 05:12:52 +0000

    finished question 2

--- quiz5/question2.py
+++ quiz5/question2.py
@@ -2,33 +2,33 @@ N = int(input("how many students? "))
 M = int(input("how many scores per student? "))
 matrix = []
 scorelist= []
+studentlist = []
 
 for i in range (N):
-  studentlist = []
-  students = input("enter name of student ")
-  studentlist.append(students)
-  for v in range (M):
-    scores = int(input("enter their scores ")) 
-    scorelist.append(scores)
-  matrix.append(scorelist)
-  
+    students = input("enter name of student ")
+    studentlist.append(students)
+    scorelist = []
+    for v in range (M):
+        scores = int(input("enter their scores ")) 
+        scorelist.append(scores)
+        print (scorelist)
+    matrix.append(scorelist)
+
 print (matrix)
 
-for x in range (N):
+for i in range (N):
   studentsum = 0
-  for j in range(M):
-    studentsum += matrix[x][j]
-    studentname = (studentlist[x])
-    print ("{0} has a total score of {1} ".format(studentname, studentsum))
-    studentavg = (studentsum/M)
-    print ("{0} has an average of {1}".format(studentname,studentavg))
+  for v in range(M):
+    studentsum += matrix[i][v]
+    studentname = (studentlist[i])
+  print ("{0} has a total score of {1} ".format(studentname, studentsum))
+  studentavg = (studentsum/M)
+  print ("{0} has an average of {1}".format(studentname,studentavg))
 
-for x in range (N):
+for i in range (N):
   scoresum = 0
-  for j in range(M):
-    scoresum += matrix[x][j]
-    subjectnum = (scorelist[x])
-    print ("{0} has a total score of {1} ",format (subjectnum, scoresum))
-    scoreavg = (scoresum/M)
-    print ("{0} has an average of {1}", format(subjectnum,scoreavg))
-    
+  for v in range(M):
+    scoresum += matrix[v][i]
+  print ("subject", i+1, "has a total score of",scoresum)
+  scoreavg = (scoresum/M)
+  print ("subject",i+1,"has an average of",scoreavg)
