df3ac6f647d03ae66787d1d3537662ec0d88f3f2
Liana Tran <98435887+lianatr@users.noreply.github.com>
2022-04-15 00:02:03 +0000

    quiz6

--- quiz6/question1.py
+++ quiz6/question1.py
@@ -9,8 +9,8 @@ def getInput():
     IDList.appened(input("enter ID number of student", i))
     studentscore= []
     for v in range (scorenum):
-      studentscore.append(input("enter score {0} of student", v)
-    matrix.append(studentscore)
+      studentscore.append(input("enter score {0} of student", v))
+    scorematrix.append(studentscore)
                           
   return namelist, IDList, scorelist
   
@@ -30,9 +30,17 @@ def FindLowest():
   return LowestScore, LowestScoreName
 
 def FindTotal():
-  
+  for i in range(studentnum):
+    studentsum = 0
+    for v in range(M):
+      studentsum += matrix[i][v]
+      studentname = (studentlist[i])
+      studentavg = (studentsum/M)
+ return studentavg,studentsum
+
 def FindAvg():
-  d
+  
+    
 
 
 def PrintAll():
